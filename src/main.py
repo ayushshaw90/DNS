7d457a19697d8136deb51759c052b8d4885c3739
AshutoshSharma343 <ashuonex@gmail.com>
2023-12-12 10:13:52 +0530

    second commit

--- src/main.py
+++ src/main.py
@@ -1,19 +1,35 @@
 import pandas as pd
-from utils.data_processing import extract_domain_features
+from utils.generate_domain_name_features import extract_domain_features
 
-def main():
-    # Load original dataset
-    df = pd.read_csv(r'data/input_data/only_non_malicious.txt')
+def main():  
     
-    #dropping all the duplicate domains
-    df = df.drop_duplicates(subset=['DomainName'])
-    # Extract features
-    df_new = df.head(10)
-    df_new['DomainFeatures'] = df_new['DomainName'].apply(extract_domain_features)
-    df_new.head()
+    # Load original dataset
+    df_normal = pd.read_csv(r'data/dnsdata_top_domains.csv')
+    df_malicious = pd.read_csv(r'data/malicious_domain_response.csv')
+    
+    df_normal['label'] = 0
+    df_malicious['label'] = 1 
+    
+    # Concatenate the dataframes
+    df = pd.concat([df_normal, df_malicious])
 
+    # Shuffle the rows
+    df = df.sample(frac=1).reset_index(drop=True)
+    df.drop('CNAME__-',axis=1,inplace=True)
+    
+    df = df.drop_duplicates(subset=['domain_name'])
+    
+    df = df.drop(['A', 'AAAA', 'MX', 'NS', 'SOA', 'CNAME', 'TXT', 'SRV', 'PTR'], axis=1)
+    
+    ###### Generating features by analysing the domain name ######
+    
+    dict = {}
+    dict = extract_domain_features('xxxxxxxxxxxxxxxxxxxxx.xmail.google.com')
+    #print(df.head(1))
+    print(dict)
+    
     # Save the new dataset
-  #  original_dataset.to_csv('data/output/new_dataset.csv', index=False)
+    # original_dataset.to_csv('data/output/new_dataset.csv', index=False)
 
 if __name__ == "__main__":
     main()
---
+++ src/utils/generate_domain_name_features.py
@@ -0,0 +1,149 @@
+import tldextract
+from collections import Counter
+import math
+
+
+def extract_domain_features(domain):
+    features = {}
+
+    # Split the domain into subdomains and TLD (Top-Level Domain)
+    ext = tldextract.extract(domain)
+    
+    print(ext)
+    print(ext.suffix)
+    print(ext.domain)
+    print(ext.subdomain)
+
+    # Full domain
+    features['full_domain'] = domain
+    
+    # Top-Level Domain
+    features['tld'] = ext.suffix if ext.suffix else None
+    
+    # Domain and Subdomain lengths
+    features['full_domain_length'] = len(domain)
+    features['domain_length'] = len(ext.domain)
+    features['subdomain_length'] = len(ext.subdomain)
+    
+    # Check if the domain has a subdomain
+    features['has_subdomain'] = 1 if ext.subdomain else 0
+    
+    #Calculating the entropy of the domain name
+    features['entropy_of_domain'] = calculate_entropy(domain)
+    
+    #Caluclating the average entropy of subdomains
+    features['avg_entropy_of_subdomains'] = calculate_average_entropy_of_subdomains(domain)
+    
+    #Calculating the ratio of alphanumeric characters in the domain name
+    features['alphanumeric_ratio'] = calculate_alphanumeric_ratio(domain)
+    
+    #Calculating the number of subdomains
+    features['subdomains_count'] = count_number_of_subdomains(domain)
+    
+    #Calculating the average length of subdomains
+    features['avg_subdomain_length'] = calculate_average_subdomain_length(domain)
+    
+    #calculating the ratio of numeric characters in the domain name
+    features['numeric_ratio'] = calculate_numeric_ratio(domain)
+    
+    #Calculating the ratio of special characters in the domain name
+    features['special_char_ratio'] = calculate_special_char_ratio(domain)
+    
+
+    return features
+
+################################################################################################
+
+def calculate_entropy(domain):
+    # Count the frequency of each character in the domain
+    freq = Counter(domain)
+    
+    # Calculate the probability of each character
+    domain_length = len(domain)
+    probabilities = [float(freq[char]) / domain_length for char in set(domain)]
+    
+    # Calculate the entropy using the Shannon entropy formula
+    entropy = -sum(p * math.log2(p) for p in probabilities)
+    
+    return entropy
+
+def calculate_average_entropy_of_subdomains(domain):
+    # Split the domain into subdomains using the dot separator
+    subdomains = domain.split('.')
+    
+    if(len(subdomains) == 2):
+        return 0
+    
+    # Calculate the entropy of each subdomain
+    subdomain_entropies = [calculate_entropy(sub) for sub in subdomains]
+    
+    # Calculate the average entropy of subdomains
+    avg_entropy = sum(subdomain_entropies)/len(subdomain_entropies)
+    
+    return avg_entropy
+
+def calculate_alphanumeric_ratio(domain):
+    # Count the number of alphanumeric characters (lowercase alphabetic or numeric)
+    alphanumeric_count = sum(1 for char in domain if char.isalnum() and char.islower())
+    
+    # Count the total number of characters in the domain
+    total_characters = len(domain)
+
+    alphanumeric_ratio = alphanumeric_count / total_characters
+    
+    return alphanumeric_ratio
+
+def count_number_of_subdomains(domain):
+    # Split the domain into subdomains using the dot separator
+    subdomains = domain.split('.')
+    
+    if(len(subdomains) <= 2):
+        return 0
+    else:
+        return len(subdomains) -2
+
+def calculate_average_subdomain_length(domain):
+    # Split the domain into subdomains using the dot separator
+    subdomains = domain.split('.')
+    
+    if(len(subdomains) <= 2):
+        return 0
+
+    # Exclude TLD and second-level domain (last two segments)
+    subdomains_to_consider = subdomains[:-2] 
+
+    # Calculate the length of each subdomain
+    subdomain_lengths = [len(sub) for sub in subdomains_to_consider]
+
+    avg_subdomain_length = sum(subdomain_lengths)/len(subdomain_lengths)
+
+    return avg_subdomain_length
+
+def calculate_numeric_ratio(domain):
+    # Calculate the number of numeric characters in the domain
+    numeric_count = sum(1 for char in domain if char.isdigit())
+    
+    # Calculate the total length of the domain
+    total_length = len(domain)
+    
+    if(total_length == 0):
+        return 0
+    
+    numeric_ratio = numeric_count / total_length
+    
+    return numeric_ratio
+
+def calculate_special_char_ratio(domain):
+    # Count the number of special characters in the domain excluding '.'
+    special_char_count = sum(1 for char in domain if not (char.isalnum() or char == '.'))
+
+    # Calculate the total length of the domain excluding '.'
+    total_length_excluding_dot = len([char for char in domain if char != '.'])
+
+    # Calculate the special character ratio
+    if total_length_excluding_dot > 0:
+        special_char_ratio = special_char_count / total_length_excluding_dot
+    else:
+        special_char_ratio = 0.0  # Set ratio to 0 if the domain has no characters excluding '.'
+
+    return special_char_ratio
