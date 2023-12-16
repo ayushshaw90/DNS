e4a93fa08cf71d1d59b343261c9c97acd0b15857
ishikapoddar <ishikapoddar30@gmail.com>
2023-12-16 22:05:17 +0530

    completed design of details

--- Frontend/dns1/pages/index.js
+++ Frontend/dns1/pages/index.js
@@ -28,22 +28,22 @@ export default function Index() {
           <div className='flex flex-col pl-48 pr-8 pt-4 gap-12  '>
             <h1 className='text-white text-xl font-semibold'>Dashboard</h1>
             <div className='grid grid-cols-3 gap-8 overflow-hidden'> 
-              <div className=" text-white  h-[24rem] bg-cover bg-no-repeat rounded-lg bg-[url('https://res.cloudinary.com/ddtzbznea/image/upload/v1702465492/cardimgfree_wr2cmr.png')]">
+              <div className=" text-white shadow-lg shadow-black  h-[24rem] bg-cover bg-no-repeat rounded-lg bg-[url('https://res.cloudinary.com/ddtzbznea/image/upload/v1702465492/cardimgfree_wr2cmr.png')]">
                 <h1> Welcome! </h1>
                 <h1>To website name</h1>
               </div>
-              <div className="rounded-lg  bg-cover bg-no-repeat bg-[url('https://res.cloudinary.com/ddtzbznea/image/upload/v1702463464/background-card-reports_hwlwq5.png')]"> 
+              <div className=" yo rounded-lg  shadow-lg shadow-black"> 
               <div className='pt-2'>
                 <h1 className='text-center text-white'>Blacklisted Domains</h1>
                 </div>
               </div>
-              <div className="rounded-lg h-[24rem] bg-cover bg-no-repeat bg-[url('https://res.cloudinary.com/ddtzbznea/image/upload/v1702463464/background-card-reports_hwlwq5.png')]"> 
+              <div className=" yo rounded-lg h-[24rem] shadow-lg shadow-black"> 
               <div className='pt-2'>
                 <h1 className='text-center text-white'>Whitelisted Domains</h1>
                 </div>
               </div>
               </div>
-              <div className="rounded-lg  bg-cover bg-no-repeat bg-[url('https://res.cloudinary.com/ddtzbznea/image/upload/v1702463464/background-card-reports_hwlwq5.png')]"> 
+              <div className="yo rounded-lg  shadow-lg shadow-black"> 
               <div className='pt-2'>
                 <h1 className='text-center text-white border-b'>Blacklisted Domains</h1>
                 <div className='flex pt-2 justify-around text-white'>
